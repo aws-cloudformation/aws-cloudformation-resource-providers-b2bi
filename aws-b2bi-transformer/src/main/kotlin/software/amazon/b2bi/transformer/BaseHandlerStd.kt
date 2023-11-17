25cbccdaf467df6c27f759f41906d8149022ac03
Nash Solon <nashsolo@amazon.com>
2023-11-17 12:58:33 -0500

    Initializes transformer resource, adds transformers smithy, and adds local b2bi sdk dependency, converts files to kotlin

---
+++ aws-b2bi-transformer/src/main/kotlin/software/amazon/b2bi/transformer/BaseHandlerStd.kt
@@ -0,0 +1,34 @@
+package software.amazon.b2bi.transformer
+
+
+import software.amazon.awssdk.core.SdkClient
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy
+import software.amazon.cloudformation.proxy.Logger
+import software.amazon.cloudformation.proxy.ProgressEvent
+import software.amazon.cloudformation.proxy.ProxyClient
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest
+
+abstract class BaseHandlerStd : BaseHandler<CallbackContext?>() {
+    override fun handleRequest(
+            proxy: AmazonWebServicesClientProxy,
+            request: ResourceHandlerRequest<ResourceModel>,
+            callbackContext: CallbackContext?,
+            logger: Logger
+    ): ProgressEvent<ResourceModel, CallbackContext?> {
+        return handleRequest(
+                proxy,
+                request,
+                callbackContext ?: CallbackContext(),
+                proxy.newProxy(ClientBuilder::getClient),
+                logger
+        )
+    }
+
+    internal abstract fun handleRequest(
+            proxy: AmazonWebServicesClientProxy,
+            request: ResourceHandlerRequest<ResourceModel>,
+            callbackContext: CallbackContext?,
+            proxyClient: ProxyClient<SdkClient>,
+            logger: Logger
+    ): ProgressEvent<ResourceModel, CallbackContext?>
+}
---
+++ aws-b2bi-transformer/src/main/kotlin/software/amazon/b2bi/transformer/Configuration.kt
@@ -0,0 +1,3 @@
+package software.amazon.b2bi.transformer
+
+class Configuration : BaseConfiguration("aws-b2bi-transformer.json")
